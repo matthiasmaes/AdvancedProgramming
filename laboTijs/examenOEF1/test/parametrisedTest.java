9de2985e9d9149682a719f1086547cdf699dfa2b
tijs sengier <tijs.sengier@student.odisee.be>
2017-01-12 16:54:10 +0100

    geparametriseerde test WERKT

--- laboTijs/examenOEF1/test/parametrisedTest.java
+++ laboTijs/examenOEF1/test/parametrisedTest.java
@@ -1,54 +1,62 @@
+
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
-import java.lang.reflect.Parameter;
-import java.util.Arrays;
-import java.util.Collection;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import static org.junit.Assert.*;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
 /**
  *
  * @author tijs
  */
 @RunWith(Parameterized.class)
 public class parametrisedTest {
+    public Map<String, String> map = new HashMap<String,String>();
     
-    public parametrisedTest() {
+    public parametrisedTest(int input, String expected) {
+        fInput= input;
+        fExpected= expected;
     }
     
     @Parameters
     public static Collection<Object[]> data() {
         return Arrays.asList(new Object[][] { 
-            { "2", "2" },
-            { "3", "Tic Tic" }, 
-            { "5", "Tac Tac" },   
-            { "7", "Toe" }, 
-            { "9", "Tic" },
-            { "10", "Tac" },
-            { "15", "Tic Tac Tac" },
-            { "35", "Tic Tac Tac Toe" } 
+            { 2, "2" },
+            { 3, "Tic Tic" }, 
+            { 5, "Tac Tac" },   
+            { 7, "Toe Toe" }, 
+            { 9, "Tic" },
+            { 10, "Tac" },
+            { 15, "Tic Tac Tac" },
+            { 35, "Tic Tac Tac Toe" } 
         });
     }
 
-    @Parameter // first data value (0) is default
-    public /* NOT private */ int fInput;
+    
+    private int fInput;
 
-    @Parameter(value = 1)
-    public /* NOT private */ int fExpected;
+    private String fExpected;
 
     @Test
     public void test() {
-        assertEquals(fExpected, Fibonacci.compute(fInput));
-    }
-    
+        map.put("3","Tic");
+        map.put("5","Tac");
+        map.put("7","Toe");
+        
+        TicTacToe all = new TicTacToe(0,100,map);
+        assertEquals(fExpected, all.DivideAndReplaceMultipleNumbers(fInput));
+    }  
 }
