6da7f5de1cdc280406f5cd2475f723b36afb406a
tijs sengier <tijs.sengier@student.odisee.be>
2017-01-14 15:30:40 +0100

    sorted map implementatie

--- laboTijs/examenOEF2/src/EindwerkCollectie.java
+++ laboTijs/examenOEF2/src/EindwerkCollectie.java
@@ -3,6 +3,11 @@ import java.util.ArrayList;
 import static java.util.Arrays.sort;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.SortedSet;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -15,13 +20,20 @@ import java.util.List;
  * @author tijs
  */
 public class EindwerkCollectie implements EindwerkCollectieInterface {
-    /****dubbele lijst per richting***/
+    public  SortedMap<String, SortedSet<Eindwerk>> map;
+    
+    public EindwerkCollectie(){
+        this.map = new TreeMap<String, SortedSet<Eindwerk>>();
+    }
+    
+    /****dubbele lijst per richting**************************************************************************
     public List<ArrayList<Eindwerk>> eindwerkcollectie;
     
     public EindwerkCollectie(){
         this.eindwerkcollectie = new ArrayList<ArrayList<Eindwerk>>();
     }
-    
+    *********************************************************************************************************************/
+   
     /****enkele list****************************************************************************************************
     public List<Eindwerk> eindwerkcollectie;
 
@@ -33,7 +45,15 @@ public class EindwerkCollectie implements EindwerkCollectieInterface {
     @Override
     public Eindwerk[] getEindwerkenVanOpleiding(String opleiding) {
         
-        /****dubbele lijst per richting***/
+        for(Map.Entry<String, SortedSet<Eindwerk>> entry : map.entrySet()) {
+            String key = entry.getKey();
+            SortedSet<Eindwerk> value = entry.getValue();
+            
+            if(key.equals(opleiding)) return value.toArray(new Eindwerk[value.size()]);
+        }
+        return null;
+        
+        /****dubbele lijst per richting************************************************
         for(int i = 0; i<eindwerkcollectie.size(); i++){
             if(!eindwerkcollectie.get(i).isEmpty() && eindwerkcollectie.get(i).get(0).opleiding.equals(opleiding)){
                 Eindwerk[] temp;
@@ -43,6 +63,7 @@ public class EindwerkCollectie implements EindwerkCollectieInterface {
             }
         }
         return null;
+        *********************************************************************************************************************/
         
         /****enkele list****************************************************************************************************
         List<Eindwerk> newList = new ArrayList<Eindwerk>();
@@ -59,26 +80,49 @@ public class EindwerkCollectie implements EindwerkCollectieInterface {
 
     @Override
     public void verwijder(Eindwerk eindwerk) {
+        for(Map.Entry<String, SortedSet<Eindwerk>> entry : map.entrySet()) {
+            String key = entry.getKey();
+            SortedSet value = entry.getValue();
+            
+            if(key.equals(eindwerk.opleiding )&& value.contains(eindwerk)) value.remove(eindwerk);
+            if(value.isEmpty()) map.remove(key);
+        }
+         
         /****enkele list****************************************************************************************************
         this.eindwerkcollectie.remove(eindwerk);
         *********************************************************************************************************************/
         
-        /****dubbele lijst per richting***/
+        /****dubbele list****************************************************************************************************
         for(int i = 0; i<eindwerkcollectie.size(); i++){
             if(!eindwerkcollectie.get(i).isEmpty() && eindwerkcollectie.get(i).get(0).opleiding.equals(eindwerk.opleiding)){
                 if (this.eindwerkcollectie.get(i).contains(eindwerk)) this.eindwerkcollectie.get(i).remove(eindwerk);
                 return;
             }
         }
+        * ***********************************************************************************************************************/
     }
 
     @Override
-    public void voegToe(Eindwerk eindwerk) { 
+    public void voegToe(Eindwerk eindwerk) {
+        for(Map.Entry<String, SortedSet<Eindwerk>> entry : map.entrySet()) {
+            String key = entry.getKey();
+            SortedSet value = entry.getValue();
+
+            if(key.equals(eindwerk.opleiding) && !value.contains(eindwerk)) value.add(eindwerk);
+        }
+
+        if(!map.containsKey(eindwerk.opleiding)){
+            SortedSet<Eindwerk> ewl = new TreeSet<Eindwerk>();
+            ewl.add(eindwerk);
+            map.put(eindwerk.opleiding, ewl);
+        }
+
+        
         /****enkele list****************************************************************************************************
         if (!this.eindwerkcollectie.contains(eindwerk)) this.eindwerkcollectie.add(eindwerk);
         *********************************************************************************************************************/
         
-        /****dubbele lijst per richting***/
+        /****dubbele lijst per richting***************************************************************************************
         for(int i = 0; i<eindwerkcollectie.size(); i++){
             if(!eindwerkcollectie.get(i).isEmpty() && eindwerkcollectie.get(i).get(0).opleiding.equals(eindwerk.opleiding)){
                 if (!this.eindwerkcollectie.get(i).contains(eindwerk)) this.eindwerkcollectie.get(i).add(eindwerk);
@@ -89,5 +133,6 @@ public class EindwerkCollectie implements EindwerkCollectieInterface {
         ArrayList<Eindwerk> ewl = new ArrayList<>();
         ewl.add(eindwerk);
         eindwerkcollectie.add(ewl);
+        * ********************************************************************************************************************/
     } 
 }
